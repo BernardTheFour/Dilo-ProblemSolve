e28b142a8c21d14991c81386c2db562199b08c4e
BernardTheFour <bernardctxy1@gmail.com>
2022-01-02 00:44:28 +0700

    upload problem 2

---
+++ Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody;
+
+    private void Awake()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody.velocity = new Vector2(2, 5);
+    }
+}
